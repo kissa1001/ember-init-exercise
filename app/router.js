05bd25be33d3880480ae6edc4411080e6594d5b1
kissa1001 <kissa101295@gmail.com>
2016-08-03 22:55:50 -0400

    added routes

--- app/router.js
+++ app/router.js
@@ -7,6 +7,12 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('orders', function(){
+      this.route('order', {path: '/:order_id'});
+  });
+  this.route('polls', function(){
+    this.route('poll', { path: '/:poll_id'} );
+  });
 });
 
 export default Router;
